38722fa4f9ec3f6d00c5ca751ac3cf5cb731fd8e
Vjatseslav Gedrovits <vjatseslav.gedrovits@gmail.com>
2022-12-09 03:43:44 +0200

    feat: added placeholder screens and routes nav

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import gameReducer from '../features/game/gameSlice';
+import mainMenuReducer from '../features/main-menu/mainMenuSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    game: gameReducer,
+    mainMenu: mainMenuReducer
   },
 });
 
