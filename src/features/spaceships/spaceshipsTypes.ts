75d08b09fdc908e99a13b6651ec209917ac384bd
Vjatseslav Gedrovits <vjatseslav.gedrovits@gmail.com>
2022-12-10 03:34:53 +0200

    feat: added battleships state for experiments

---
+++ src/features/spaceships/spaceshipsTypes.ts
@@ -0,0 +1,17 @@
+export enum HullSize {
+  Tiny = 'Tiny',
+  Small = 'Small',
+  Medium = 'Medium',
+  Large = 'Large',
+  Huge = 'Huge',
+  Collosal = 'Collosal',
+}
+
+export enum HullType {
+  Shuttle = 'Shuttle',
+  Frigate = 'Frigate',
+  Destroyer = 'Destroyer',
+  Cruiser = 'Cruiser',
+  Battleship = 'Battleship',
+  Carrier = 'Carrier',
+}
