4c5b918b067b81364a08ae7daedf4c7b08907b4c
Vjatseslav Gedrovits <vjatseslav.gedrovits@gmail.com>
2022-12-11 03:21:37 +0200

    feat: added battle UI with separated ship state

---
+++ src/features/battle/RightSpaceship.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { HullSize, HullType } from '../spaceships/spaceshipsTypes';
+
+import styles from '../spaceships/Spaceships.module.css';
+import logo from '../../logo.svg';
+
+export function RightSpaceship() {
+  const dispatch = useAppDispatch();
+  const { rightSpaceship } = useAppSelector((state) => state.battle);
+
+  return (
+    <div>
+      <h1>Spaceship!</h1>
+      {rightSpaceship && (
+        <div className={styles.card} key={rightSpaceship.id}>
+          <img src={logo} className="App-logo" alt="Spaceship" style={{width: '50%'}} />
+          <div className={styles.container}>
+            <h4>ID: {rightSpaceship.id}</h4>
+            <p>Size: {HullSize[rightSpaceship.hullSize]}</p>
+            <p>Type: {HullType[rightSpaceship.hullType]}</p>
+            <p style={{backgroundColor: "red"}}>Hit Points: {rightSpaceship.hitPoints}/{rightSpaceship.maxHitPoints}</p>
+            <p style={{backgroundColor: "yellow"}}>Armor Points: {rightSpaceship.armorPoints}/{rightSpaceship.maxArmorPoints}</p>
+            <p style={{backgroundColor: "blue"}}>Shield Points: {rightSpaceship.shieldPoints}/{rightSpaceship.maxShieldPoints}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
